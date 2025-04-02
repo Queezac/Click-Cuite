1a1618faa07970c30747715cbceea477b978fd43
Abiven <salome.abiven@etudiant.univ-rennes1.fr>
2025-04-02 18:38:38 +0200

    css encore

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function App() {
           <ul className="list-none p-0">
             {upgradeList.map((upgrade) => {
               const currentQuantity =
-              useGameStore.getState().upgrades[upgrade.id] || 0;
+                useGameStore.getState().upgrades[upgrade.id] || 0;
               const cost = Math.floor(
                 upgrade.baseCost * Math.pow(upgrade.upgradeCostMultiplier, currentQuantity)
               );
@@ -95,22 +95,24 @@ function App() {
                   key={upgrade.id}
                   onClick={() => buyUpgrade(upgrade.id)}
                   disabled={alcoholCount < cost}
-                  className={`w-full px-3 py-2 mt-2 rounded-[10px] text-white flex justify-between items-center ${
-                    alcoholCount >= cost ? "bg-[#14191F] cursor-pointer" : "bg-[#313B47]"
-                  }`}
+                  className={`w-full px-3 py-2 mt-2 rounded-[10px] text-white flex justify-between items-center 
+                    ${alcoholCount >= cost 
+                      ? "bg-[#14191F] cursor-pointer active:bg-[#0d141b]"  // Darker background on active
+                      : "bg-[#313B47]"
+                    }`}
                 >
                   <div className="flex items-center gap-2 w-[60%]">
                     <img
                       src={upgrade.image}
                       alt={upgrade.name}
-                      className={`w-10 h-10 flex-shrink-0 ${alcoholCount < cost ? 'disabled' : ''}`}
+                      className={`w-10 h-10 flex-shrink-0 ${alcoholCount < cost ? 'opacity-50' : ''}`}
                     />
-                    <p className={`text-[0.7vw] truncate ${alcoholCount < cost ? 'disabled' : ''}`}>{upgrade.name}</p>
+                    <p className={`text-[0.7vw] truncate ${alcoholCount < cost ? 'opacity-50' : ''}`}>{upgrade.name}</p>
                   </div>
 
                   <div className="flex items-center w-[38%]">
-                    <strong className={`text-[#5F6EFF] text-center w-[30%] ${alcoholCount < cost ? 'disabled' : ''}`}>{currentQuantity}</strong>
-                    <strong className={`text-[0.8vw] text-right w-[70%] ml-auto ${alcoholCount < cost ? 'disabled' : ''}`}>{conversionUtils.mLToString(cost)}</strong>
+                    <strong className={`text-[#5F6EFF] text-center w-[30%] ${alcoholCount < cost ? 'opacity-50' : ''}`}>{currentQuantity}</strong>
+                    <strong className={`text-[0.8vw] text-right w-[70%] ml-auto ${alcoholCount < cost ? 'opacity-50' : ''}`}>{conversionUtils.mLToString(cost)}</strong>
                   </div>
                 </button>
               );
